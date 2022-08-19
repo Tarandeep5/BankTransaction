31ddd5cb45a8bd52c7cf8b5129f3e151a7a3e3ed
Tarandeep5 <tarandeepsingh7837420@gmail.com>
2022-08-19 20:39:37 +0530

    CRUD operations With asp.net
    Learned the Scaffolding technique of C# .NET framework while making CRUD operations.

---
+++ Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransaction.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName="nvarchar(12)")]
+        public String AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public String BenificiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public String BankName { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+        public String SwiftCode { get; set; }
+        public int Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using BankTransaction.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//depnedencyInjecion for dbContext
+
+builder.Services.AddDbContext<TransactionDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
 
 var app = builder.Build();
 
@@ -13,6 +21,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
 
 app.UseAuthorization();
 
